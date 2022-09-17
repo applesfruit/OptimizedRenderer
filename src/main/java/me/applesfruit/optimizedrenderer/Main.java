6f37789756e15e99164420e42ed24ab3b3700360
applesfruit <83845564+applesfruit@users.noreply.github.com>
2022-09-17 16:11:52 -0700

    a
    a

---
+++ src/main/java/me/applesfruit/optimizedrenderer/Main.java
@@ -0,0 +1,11 @@
+package me.applesfruit.optimizedrenderer;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(version = "0.1", name = "Optimized Renderer", modid = "optimizedrenderer")
+public class Main {
+
+
+
+
+}
